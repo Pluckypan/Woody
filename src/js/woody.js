248c884a908dc18eca73744a5702867bccc036f7
plucky <plucky.pan@ubnt.com>
2018-01-26 22:36:31 +0800

    实现登录Gist和Token持久化

--- src/js/woody.js
+++ src/js/woody.js
@@ -3,5 +3,14 @@
 function login(gist,token){
   localStorage.GIST_ID=gist
   localStorage.PERSONAL_TOKEN=token
-  location.href="main.html"
+  location.href="index.html"
+}
+
+// 获取Gist
+function getGist(){
+  return localStorage.GIST_ID;
+}
+
+function getToken(){
+  return localStorage.PERSONAL_TOKEN;
 }
